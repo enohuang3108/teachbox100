33d02ded60d06e18941fa394376e36d6daa0f137
eno.huang <enohuang3108@gmail.com>
2025-08-20 21:25:13 +0800

    ♻️ refactor: rename coin transaction route to coin buy route
    - Rename app/coin/transaction/ to app/coin/buy/
    - Remove transaction page and opengraph-image components
    - Update cover image filename from coin_transaction to coin_buy
    - Simplify route structure for better clarity

--- app/coin/transaction/opengraph-image.tsx
+++
@@ -1,9 +0,0 @@
-import { generateOgImageResponse, getOgImageMetadata } from "@/lib/og-image";
-
-const pageKey = "coin-transaction";
-
-export const { alt, size, contentType } = getOgImageMetadata(pageKey);
-
-export default async function Image() {
-  return generateOgImageResponse(pageKey);
-}
